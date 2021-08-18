c6986c99cff19dd0f8b1329d6191027e01d2ba34
Sahil Gulati <64752326+sahil150395@users.noreply.github.com>
2021-08-18 05:59:38 +0000

    Test case passed

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ age = input("What is your current age?")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
+yearsLeft = 90 - int(age)
+daysLeft = yearsLeft * 365
+weeksLeft = yearsLeft * 52
+monthsLeft = yearsLeft * 12
+
+print(f"You have {daysLeft} days, {weeksLeft} weeks, and {monthsLeft} months left")
 
 
 
